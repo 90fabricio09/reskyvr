fbafae56eaf845b73feab47b2987dc406062260a
90fabricio09 <90934129+90fabricio09@users.noreply.github.com>
2024-04-05 17:48:01 -0300

    Update contagem.js

--- js/contagem.js
+++ js/contagem.js
@@ -34,7 +34,7 @@ const contagemRegressiva = (tempo) => {
 
 const tempoRestante = () => {
     // 1 de janeiro de 1970
-    const dataEvento = new Date ('2025-11-01 00:00:00');
+    const dataEvento = new Date ('2025-01-11 00:00:00');
     const hoje = Date.now();
     return Math.floor((dataEvento - hoje) / 1000);
 }
